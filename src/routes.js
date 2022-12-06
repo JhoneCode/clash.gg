ec054288956225607eafb4a709346708ba09b363
JhoneCode <joao.pedro100@hotmail.com.br>
2022-12-06 20:49:00 -0300

    prettier eslint setup

--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,25 @@
 
+const { Client, BatchThrottler } = require('clashofclans.js')
+const client = new Client({
+  cache: true,
+  retryLimit: 1,
+  restRequestTimeout: 5000,
+  throttler: new BatchThrottler(20)
+})
+
+client.events.addClans(['#2LC88PYUG'])
+client.events.setClanEvent({
+  name: 'clanDescriptionChange',
+  filter: (oldClan, newClan) => {
+    return oldClan.description !== newClan.description
+  }
+})
+
+client.on('clanDescriptionChange', (oldClan, newClan) => {
+  console.log(oldClan.description, newClan.description)
+});
+
+(async function () {
+  await client.login({ email: 'joaopedro.castrovieira@gmail.com', password: 'Jp20Cv2022@' })
+  await client.events.init()
+})()
