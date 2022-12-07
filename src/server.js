5d7642a62c522b1a097d784081d09efe74e51bd7
JhoneCode <joao.pedro100@hotmail.com.br>
2022-12-07 08:27:41 -0300

    lib poc

--- src/server.js
+++ src/server.js
@@ -1,1 +1,11 @@
+require('dotenv').config()
+const { Client, QueueThrottler } = require('clashofclans.js')
 
+const client = new Client({
+  keys: [process.env.API_KEY],
+  retryLimit: 1,
+  restRequestTimeout: 5000,
+  throttler: new QueueThrottler(100)
+})
+
+console.log(client.events)
